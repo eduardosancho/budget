771ae71920a9ed5bd25dc6297b9de93b4ea298b8
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-24 14:10:13 -0600

    Test a request to login page

---
+++ spec/requests/login_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Devise', type: :request do
+  describe 'GET /sign_in' do
+    subject { User.new(name: 'Jacky', email: 'jacky@email.com', password: '123456', password_confirmation: '123456') }
+
+    before do
+      subject.skip_confirmation!
+      subject.save
+    end
+
+    before(:example) do
+      get new_user_session_path
+    end
+
+    it 'request is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'response body has expected title text' do
+      expect(response.body).to include('Log in')
+    end
+  end
+end
