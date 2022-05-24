5dc1f5dc46c35a8a0f8fe85a9750246c60cd7ede
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-24 12:25:58 -0600

    Add authenticate_user before any actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "categories#index"
+  devise_scope :user do
+    authenticated :user do
+      root "categories#index", as: :authenticated_root
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
