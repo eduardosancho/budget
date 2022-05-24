f3ede8a7b37caff5d3e60a85de6d180ffa217675
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-24 17:52:41 -0600

    Style splash creen

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,6 @@ class CategoriesController < ApplicationController
   skip_before_action :authenticate_user!, only: :new_splash
 
   def new_splash
-    render 'devise/sessions/splash'
+    render 'devise/sessions/splash', layout: 'splash'
   end
 end
