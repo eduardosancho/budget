e83e470a00266e43a3f4ffd2cb12e28ff71c01ba
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-24 10:29:21 -0600

    Adjust manytomany association thru categorizations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
   belongs_to :user
+
+  has_many :categorizations
+  has_many :operations, through: :categorizations
+
   validates :name, presence: true
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,4 +1,8 @@
 class Operation < ApplicationRecord 
   belongs_to :author, class_name: 'User'
+
+  has_many :categorizations
+  has_many :categories, through: :categorizations
+
   validates :name, presence: true
 end
