4ec04601d04e52338120e9fc7983153d67933e00
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-24 12:35:19 -0600

    Fix user_spec, broken by devise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Test validations' do
-    subject { User.new(name: 'Arnold') }
+    subject { User.new(name: 'Arnold', email: 'arnold@email.com', password: '123456', password_confirmation: '123456') }
 
-    before { subject.save }
+    before { 
+      subject.skip_confirmation!
+      subject.save 
+    }
 
     it 'should be valid if the name contains text' do
       expect(subject).to be_valid
@@ -12,6 +15,18 @@ RSpec.describe User, type: :model do
 
     it 'should not be valid if name is nil' do
       subject.name = nil
+      expect(subject).to_not be_valid
+    end
+    
+    it 'should not be valid if email is nil' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+    
+    it 'should not be valid if password has less than 6 characters' do
+      subject.password = 'abc'
+      expect(subject).to_not be_valid
+      subject.password = '54321'
       expect(subject).to_not be_valid
     end
   end
