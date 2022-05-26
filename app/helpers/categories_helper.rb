e7d29e6a140c87d4b01bb8d26720eaa58bf072e3
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-25 22:47:08 -0600

    Add message when user has no categories

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,7 @@
 module CategoriesHelper
+  def categories?
+    out = ''
+    out << "<p class=\'align-self-center mt-5\'>This user has no categories registered</p>" if current_user.categories.empty?
+    out.html_safe
+  end
 end
