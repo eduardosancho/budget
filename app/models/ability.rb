cb98c90925820ed50749086b509d924702f0d5ee
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-26 17:08:06 -0600

    Fix bug, missing author_id

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,9 @@ class Ability
     if user.admin?
       can :manage, :all
     else
-      can :manage, :all, user_id: user.id
+      can :manage, Operation, author_id: user.id
+      can :manage, Category, user_id: user.id
+      can :manage, Categorization, user_id: user.id
     end
   end
 end
