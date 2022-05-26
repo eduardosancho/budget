5e6487eb6a28149ca1152b3d6d819c8880ff3aab
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-26 15:42:49 -0600

    Fix typo, missing comma

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer :: Base.smtp_settings = {
   user_name: 'apikey',#This is the string literal'apikey',NOT the ID of your API key
-  password: Rails.application.credentials.dig(:sendgrid, :api_key)
+  password: Rails.application.credentials.dig(:sendgrid, :api_key),
   domain: 'rocky-cliffs-67926.herokuapp.com',
   address: 'smtp.sendgrid.net',
   port: 587,
